d7a4243448b8337470c077f092630e51386cf97d
braydenminifie <brayden.m1712@gmail.com>
2026-04-19 18:29:26 +1200

    feat: added services for encounters

---
+++ server/app/services/encounter_service.py
@@ -0,0 +1,55 @@
+from ..extensions import db
+from ..models.combatant import Combatant
+from ..models.encounter import Encounter
+
+def create_encounter(name: str):
+    encounter = Encounter(
+        name = name,
+        current_round = 1,
+        current_turn_index = 0,
+        is_active = True)
+
+    db.session.add(encounter)
+    db.session.commit()
+
+    return encounter
+
+def create_combatant(
+        encounter_id: int,
+        name: str,
+        type: str,
+        initiative: int,
+        max_hp: int,
+        armour_class: int
+        ):
+    combatant = Combatant(
+        encounter_id = encounter_id,
+        name = name,
+        type = type,
+        initiative = initiative,
+        max_hp = max_hp,
+        current_hp = max_hp,
+        armour_class = armour_class,
+        is_alive = True
+    )
+    
+    db.session.add(combatant)
+    db.session.commit()
+    
+def remove_combatant(id: int):
+    combatant = db.session.query(Combatant).filter(Combatant.id == id)
+    db.session.delete(combatant)
+    db.session.commit()
+
+def remove_encounter(id: int):
+    encounter = db.session.query(Encounter).filter(Encounter.id == id)
+    db.session.delete(encounter)
+    db.session.commit()
+
+def get_encounter(id: int):
+    encounter = db.session.query(Encounter).filter(Encounter.id == id)
+    return encounter
+
+def get_combatant(id: int):
+    combatant = db.session.query(Combatant).filter(Combatant.id == id)
+    return combatant
