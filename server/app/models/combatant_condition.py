b272092aed6e649f630ee801bd354aac99d10997
braydenminifie <brayden.m1712@gmail.com>
2026-04-19 18:08:15 +1200

    feat: added combatant_condition class model

---
+++ server/app/models/combatant_condition.py
@@ -0,0 +1,16 @@
+from ..extensions import db
+
+class CombatantCondition(db.Model):
+    __tablename__ = "combatant_conditions"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    combatant_id = db.Column(db.Integer, db.ForeignKey("combatants.id"), nullable=False)
+    condition_id = db.Column(db.Integer, db.ForeignKey("conditions.id"), nullable=False)
+
+    duration_turns = db.Column(db.Integer, nullable=True)
+
+    applied_round = db.Column(db.Integer, nullable=False)
+    applied_turn_index = db.Column(db.Integer, nullable=False)
+
+    is_active = db.Column(db.Boolean, default=True)
